ffc6975ab95f9d53e40b4f81a53829d993b274b7
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2024-12-31 20:20:59 -0500

    Auth 2.0

---
+++ src/main/java/com/org/inventory/service/UserService.java
@@ -0,0 +1,15 @@
+package com.org.inventory.service;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.org.inventory.model.User;
+
+@Service
+public class UserService {
+    public User createUser(OAuth2User oAuth2User) {
+        User user = new User();
+        user.setName(oAuth2User.getAttribute("name"));
+        user.setEmail(oAuth2User.getAttribute("email"));
+        return user;
+    }
+}
